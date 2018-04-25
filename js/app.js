381c1068eb99019213375b1960ebac67fff045c0
Algirdas Mazeika <algirdaslt@gmail.com>
2018-04-25 10:42:19 +0300

    feat: add Player class

--- js/app.js
+++ js/app.js
@@ -26,12 +26,28 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+    }
 
+    update() {
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput() {
+
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
